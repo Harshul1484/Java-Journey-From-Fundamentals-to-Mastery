88373d884bd32816be1f58a701facaf0229004df
Harshul Rathod <118340577+Harshul1484@users.noreply.github.com>
2023-08-20 00:53:39 +0530

    Add files via upload

---
+++ 2.Variables/ArithmeticFunction.java
@@ -0,0 +1,18 @@
+public class ArithmeticFunction {
+    public static void main(String[] args){
+      int a = 15;
+      int b = 5;
+
+      //addition
+      int sum = a + b;
+      System.out.println("Addition: " +sum);
+
+      //substraction
+      int diff = a - b;
+      System.out.println("Substraction: " + diff);
+
+      //multiplication
+      int mul = a * b;
+      System.out.println("Multiplication: " + mul);
+    }
+}
---
+++ 2.Variables/Variables.java
@@ -0,0 +1,29 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Integral types
+        byte TypeByte = 127;          // 8-bit
+        short TypeShort = 32767;      // 16-bit
+        int TypeInt = 2147483647;     // 32-bit
+        long TypeLong = 9223372036854775807L; // 64-bit
+
+        // Floating-point types
+        float TypeFloat = 3.14f;      // 32-bit
+        double TypeDouble = 3.14159;  // 64-bit
+
+        // Character type
+        char TypeChar = 'A';          // 16-bit Unicode character
+
+        // Boolean type
+        boolean TypeBoolean = true;   // 0 or 1
+
+        // Output
+        System.out.println("byte: " + TypeByte);
+        System.out.println("short: " + TypeShort);
+        System.out.println("int: " + TypeInt);
+        System.out.println("long: " + TypeLong);
+        System.out.println("float: " + TypeFloat);
+        System.out.println("double: " + TypeDouble);
+        System.out.println("char: " + TypeChar);
+        System.out.println("boolean: " + TypeBoolean);
+    }
+}
