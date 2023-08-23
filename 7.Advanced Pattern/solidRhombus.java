29ac38090c4af545924043852c4b3d5fdca8579a
Harshul Rathod <118340577+Harshul1484@users.noreply.github.com>
2023-08-23 17:15:14 +0530

    Add files via upload

---
+++ 7.Advanced Pattern/solidRhombus.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class solidRhombus {
+   public static void main(String args[]) {
+       int n = 5;
+       for(int i=1; i<=n; i++) {
+        //spaces
+        for(int j=1; j<=n-i; j++) {
+            System.out.print(" ");
+        }
+
+
+        //stars
+        for(int j=1; j<=n; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}   
+}
+
+
